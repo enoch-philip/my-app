31c00e69a7a92e36c444136a2ccceea843def1b2
enoch-philip <enochphilipurla@gmail.com>
2025-03-27 14:36:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 import streamlit as st
 import joblib
-st.title('loan approval process automation')
-model=joblib.load('loan_data1.joblib')
-gender=st.number_input('enter your gender')
-married=st.number_input('Are you married 1,0')
-income=st.number_input('enter your income')
-la=st.number_input('enter loan ammount in thousands')
-if st.button ('loan predict'):
-    prediction =model.predict([[gender,married,income,la]])
-    if prediction=='y':
-        print('approved')
+st.title('Loan Approval Process Automation')
+model = joblib.load('loan_data1.joblib')
+gender = st.number_input('Enter your gender (1 for Male, 0 for Female)', min_value=0, max_value=1)
+married = st.number_input('Are you married? (1 for Yes, 0 for No)', min_value=0, max_value=1)
+income = st.number_input('Enter your income', min_value=0)
+loan_amount = st.number_input('Enter loan amount in thousands', min_value=0)
+if st.button('Predict Loan Approval'):
+    prediction = model.predict([[gender, married, income, loan_amount]])
+    if prediction == 'y':  
+        st.write('Loan Approved!')
     else:
-        print("not approved")
-        
+        st.write('Loan Not Approved')
